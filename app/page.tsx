b060d17507300e18e1965f94799932e896545c9d
daffabadrant9390 <daffabadrant@gmail.com>
2025-04-01 08:44:07 +0700

    feat: add Container component, SCSSS, and classNames package

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Container } from '@/components/Container/Container';
+
 export default function Home() {
   return (
-    <main className="w-screen h-screen flex flex-col gap-3 items-center justify-center">
-      <h1 className="text-green-900 font-bold">Hello World</h1>
-      <p className="text-green-800 font-semibold">Welcome to fleave.inc</p>
-    </main>
+    <Container>
+      <h1 className="text-red-600 font-bold hover:text-blue-600 transition-all duration-150">
+        Hello World
+      </h1>
+    </Container>
   );
 }
